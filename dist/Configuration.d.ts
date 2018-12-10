d520e19ebdea233f560b4884218e6145dd1b4546
Patrick Barnes <patrick@devinsites.com>
2018-12-10 11:41:26 -0800

    remove dist from repo

--- dist/Configuration.d.ts
+++
@@ -1,9 +0,0 @@
-export interface Configuration {
-    caCert: string;
-    clientCert: string;
-    privateKey: string;
-    clientId: string;
-    brokerHostname: string;
-    region: string;
-}
-export declare const readConfiguration: (filename?: string) => Configuration;
--- dist/connection/HostConnection.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class HostConnectionError extends Error {
-    constructor(message) {
-        super(message);
-    }
-}
-exports.HostConnectionError = HostConnectionError;
-//# sourceMappingURL=HostConnection.js.map
