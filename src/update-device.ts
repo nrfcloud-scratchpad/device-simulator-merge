45a9b7212a1581ef941d3cc241091a510b6ef003
Cole Hafner <colehafner@gmail.com>
2019-06-06 16:17:37 -0700

    fix: fixed bucket param in update-device

--- src/update-device.ts
+++ src/update-device.ts
@@ -13,7 +13,7 @@ const iot = new Iot();
 const main = async ({
   id,
   nextAppFwVersion,
-  bucket,
+  s3Bucket: bucket,
   endpoint,
   firmware,
 }: program.Command) => {
@@ -116,7 +116,7 @@ const main = async ({
 
 program
   .option('-d, --id <id>', 'id of the device', process.env.DEVICE_ID)
-  .option('-b, --bucket <bucket>', 'S3 bucket', process.env.S3_BUCKET)
+  .option('-b, --s3-bucket <bucket>', 'S3 bucket', process.env.BUCKET)
   .option('-f, --firmware <firmware>', 'name of the firmware file')
   .option(
     '-e, --endpoint <endpoint>',
